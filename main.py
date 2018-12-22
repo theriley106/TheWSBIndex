186fa143c38d0905ae6e1a1fc500fc8e133b4026
Theriley106 <christopherlambert106@gmail.com>
2018-12-21 20:22:25 -0500

    Added dataset read

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import csv
 
 
-def read_dataset(filename="vixcurrent.csv"):
+def read_csv(filename):
 	# Reads the dataset with historical prices
 	with open(filename, 'rb') as f:
 		reader = csv.reader(f)
 		return list(reader)
+
+def read_dataset(filename="vixcurrent.csv"):
+	csvFile = read_csv(filename)
+	# This is the csv file containing historical prices
+	csvFile = csvFile[2:]
+	# Removes the header columns
+	print len(csvFile)
+	return csvFile
+
+class Algo(object):
+	"""docstring for Algo"""
+	def __init__(self):
+		self.dataset = read_dataset()
+		# This gets the current dataset
+
+
+if __name__ == '__main__':
+	a = Algo()
