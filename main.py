5354f94d3f050289e11cd4ba9e25dc6afb34d848
Theriley106 <christopherlambert106@gmail.com>
2018-12-21 20:28:33 -0500

    Added the full read dataset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-
+COLUMNS = ['open', 'high', 'low', 'close']
 
 def read_csv(filename):
 	# Reads the dataset with historical prices
@@ -12,14 +12,27 @@ def read_dataset(filename="vixcurrent.csv"):
 	# This is the csv file containing historical prices
 	csvFile = csvFile[2:]
 	# Removes the header columns
-	print len(csvFile)
 	return csvFile
 
 class Algo(object):
 	"""docstring for Algo"""
 	def __init__(self):
-		self.dataset = read_dataset()
-		# This gets the current dataset
+		self.days = []
+		# These are the days in the dataset
+		self.dataset = {}
+		# These are the values of the dataset
+		for row in read_dataset():
+			# This gets the current dataset
+			day = row[0]
+			# This is the day value
+			self.dataset[day] = {}
+			# This contains the information from each column
+			for i, columnVal in enumerate(COLUMNS):
+				# Iterates over each column
+				self.dataset[day][columnVal] = row[i+1]
+				# Assigns each value to the info dict
+		print self.dataset
+
 
 
 if __name__ == '__main__':
