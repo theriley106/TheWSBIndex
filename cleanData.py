0130964e2eed7bfe7bb89543662e21f07a170739
Theriley106 <christopherlambert106@gmail.com>
2018-12-22 22:04:58 -0500

    Added better clean data

--- cleanData.py
+++ cleanData.py
@@ -10,13 +10,35 @@ def get_all_possible_tickers(fileName="companylist.csv"):
 		your_list = list(reader)
 	return [x[0] for x in your_list[1:]]
 
+def convert_utc_to_date(utcTime):
+	# This is currently not working properly
+	return utcTime
+
 STOCK_TICKERS = get_all_possible_tickers()
 
 def extract_tickers(string):
 	e = re.findall('[A-Z]{1,4}|\d{1,3}(?=\.)|\d{4,}', string)
 	return list(set(e).intersection(set(STOCK_TICKERS)))
 
+def read_forumn_data(filename="/media/christopher/ssd/wsbData.json"):
+	# This is the data from wallstreet bets
+	# It populates the forumnData
+	count = {}
+	with open(filename) as f:
+		for i, line in enumerate(f):
+			val = json.loads(line)
+			if val['body'] != '[deleted]':
+				for val in extract_tickers(val['body']):
+					if val not in count:
+						count[val] = 0
+					count[val] += 1
+			if i % 20000 == 0 and i != 0:
+				break
+	for key, value in count.iteritems():
+		print("{} - {}".format(key, value))
+
 
 if __name__ == '__main__':
-	messageVal = '''AMD up AH is not very cash money'''
-	print extract_tickers(messageVal)
+	#messageVal = '''AMD up AH is not very cash money'''
+	#print extract_tickers(messageVal)
+	read_forumn_data()
