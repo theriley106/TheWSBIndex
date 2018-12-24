edb72964c5323e3fe229f447c68475adf871e3bf
Theriley106 <christopherlambert106@gmail.com>
2018-12-23 23:00:16 -0500

    Moved all to scripts/ folder

--- convertDate.py
+++
@@ -1,8 +0,0 @@
-import dateparser as dp
-
-def convert_date(stringVal):
-	return str(int(round(int(stringVal),-3)))
-
-print dp.parse(convert_date(1390255095))
-#dt = dp.parse('1390255095')
-#print (dt)
--- dataVisualizations/app.py
+++ dataVisualizations/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -9,7 +10,12 @@ def index():
 
 @app.route('/viz1', methods=['GET'])
 def viz1():
-	return render_template("viz1.html")
+	a = json.load(open("static/AllCounts.json"))
+	db = []
+	for key, val in a.iteritems():
+		db.append({"Ticker": key, "Mentions": val})
+	db = sorted(db, key=lambda k: k['Mentions'])[-20:]
+	return render_template("viz1.html", DATABASE=db)
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000)
