e1b3471d3abae30b79d469a4535935497f39d890
Theriley106 <christopherlambert106@gmail.com>
2018-12-31 17:04:18 -0500

    Added inverse wsb

--- algo.py
+++ algo.py
@@ -1,4 +1,6 @@
 import main
+import json
+import traceback
 
 def by_word_count():
 	c = open("comments.txt").read().split("\n")
@@ -11,7 +13,15 @@ def by_word_countz():
 	a.run()
 
 if __name__ == '__main__':
-	for tickerVal in main.STOCK_TICKERS:
+	g = json.load(open("dataset/AllCounts.json"))
+	h = []
+	a = json.load(open('sentimentByTicker.json'))
+	for key, val in g.iteritems():
+		h.append({'count': val, 'ticker': key})
+	newlist = sorted(h, key=lambda k: k['count'])
+	i = 0
+	for tickerVal in newlist[::-1]:
+		tickerVal = tickerVal['ticker']
 		try:
 			b = main.get_diff_from_ticker(tickerVal)
 			g = main.get_percent_diff_from_ticker(tickerVal)
@@ -19,15 +29,28 @@ if __name__ == '__main__':
 			total = 0
 			success = 0
 			incorrect = 0
-			for key, value in main.calc_predicted_direction(tickerVal).iteritems():
-				if key in b and value != 0:
-					if value < 0:
-						start_amount += start_amount * (-1*g[key])
-					else:
-						start_amount += start_amount * g[key]
+			h = json.load(open("dataset/ListOfDatesOrder.json"))
+			directions = main.calc_predicted_direction(tickerVal)
+			for val in h:
+				if val not in b:
+					h.remove(val)
+			for i in range(14,len(h)):
+				prevKey = h[i-14]
+				key = h[i]
+				if key in b:
+					if directions[prevKey] != 0:
+						#raw_input(g[key])
+						if directions[prevKey] > 0:
+							start_amount += (start_amount * ((-1*g[key])*.01))
+						else:
+							start_amount += (start_amount * (g[key]*.01))
 			"""print("Correct: {}".format(success))
 						print("Incorrect: {}".format(incorrect))
 						print("Total: {}".format(total))"""
-			print("{} - {}".format(tickerVal, (start_amount/1000000.0)*100))
-		except:
+			if start_amount - 1000000 > 0:
+				print("{} - +{}".format(tickerVal, start_amount - 1000000))
+			else:
+				print("{} - {}".format(tickerVal, (start_amount - 1000000)))
+		except Exception as exp:
+			#traceback.print_exc()
 			pass
