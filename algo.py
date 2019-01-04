35971b6f1f8fb0f2000f15a38fbd4a27344df8f2
Theriley106 <christopherlambert106@gmail.com>
2019-01-03 19:00:08 -0500

    Added strategies

--- algo.py
+++ algo.py
@@ -24,7 +24,7 @@ def strategy0(tradeClass):
 
 def strategy1(tradeClass):
 	description = """Trading based off of changes in ticker mention frequency"""
-	strategy = {'trades': {}, 'delay': 0, "description": description}
+	strategy = {'trades': {}, 'delay': 3, "description": description}
 	for date, ratio in tradeClass.ratio_by_date.iteritems():
 		diffVal = (ratio - tradeClass.average_ratio)
 		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
@@ -33,6 +33,68 @@ def strategy1(tradeClass):
 			if diffVal > 0:
 				strategy['trades'][date] = {"trade": "long"}
 			else:
+				#strategy['trades'][date] = {"trade": "long"}
+				strategy['trades'][date] = {"trade": "short"}
+	return strategy
+
+def strategy3(tradeClass):
+	description = """Trading based off of changes in ticker mention frequency"""
+	strategy = {'trades': {}, 'delay': 3, "description": description}
+	for date, ratio in tradeClass.ratio_by_date.iteritems():
+		diffVal = (ratio - tradeClass.average_ratio)
+		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
+			strategy['trades'][date] = {"trade": None}
+		else:
+			strategy['trades'][date] = {"trade": "short"}
+	return strategy
+
+def strategy5(tradeClass):
+	description = """Trading based off of changes in ticker mention frequency"""
+	strategy = {'trades': {}, 'delay': 3, "description": description}
+	for date, ratio in tradeClass.ratio_by_date.iteritems():
+		diffVal = (ratio - tradeClass.average_ratio)
+		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
+			strategy['trades'][date] = {"trade": None}
+		else:
+			strategy['trades'][date] = {"trade": "long"}
+	return strategy
+
+def strategy6(tradeClass):
+	description = """Trading based off of changes in ticker mention frequency"""
+	strategy = {'trades': {}, 'delay': 3, "description": description}
+	for date, ratio in tradeClass.ratio_by_date.iteritems():
+		diffVal = (ratio - tradeClass.average_ratio)
+		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
+			strategy['trades'][date] = {"trade": "long"}
+		else:
+			strategy['trades'][date] = {"trade": None}
+	return strategy
+
+def strategy7(tradeClass):
+	description = """Trading based off of changes in ticker mention frequency"""
+	strategy = {'trades': {}, 'delay': 3, "description": description}
+	for date, ratio in tradeClass.ratio_by_date.iteritems():
+		diffVal = (ratio - tradeClass.average_ratio)
+		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
+			strategy['trades'][date] = {"trade": "short"}
+		else:
+			strategy['trades'][date] = {"trade": None}
+	return strategy
+
+def strategy4(tradeClass):
+	description = """Trading based off of changes in ticker mention frequency"""
+	strategy = {'trades': {}, 'delay': 3, "description": description}
+	prevTrade = None
+	for date, ratio in tradeClass.ratio_by_date.iteritems():
+		diffVal = (ratio - tradeClass.average_ratio)
+		if abs(diffVal / tradeClass.average_ratio) * 100 < 25:
+			strategy['trades'][date] = {"trade": prevTrade}
+		else:
+			if diffVal > 0:
+				prevTrade = "long"
+				strategy['trades'][date] = {"trade": "long"}
+			else:
+				prevTrade = "short"
 				strategy['trades'][date] = {"trade": "short"}
 	return strategy
 
@@ -46,9 +108,45 @@ def strategy2(tradeClass):
 
 
 if __name__ == '__main__':
-	a = main.Trade("TSLA")
-	startAmount = 1000000
-	buyAndHold = a.calc_buy_and_hold(startAmount)
-	strat1 = a.test_strategy(strategy1, startAmount)
-	print("Returns from buy and hold: ${:,.2f}".format(buyAndHold-startAmount))
-	print("Returns from Strategy 1: ${:,.2f}".format(strat1-startAmount))
+	infoz = {'buyAndHold': 0}
+	for stock in main.STOCK_TICKERS:
+		try:
+			if main.get_total_count_by_ticker(stock) > 1000:
+				a = main.Trade(stock)
+				startAmount = 1000000
+				buyAndHold = a.calc_buy_and_hold(startAmount)-startAmount
+				strat1 = a.test_strategy(strategy1, startAmount)-startAmount
+				strat3 = a.test_strategy(strategy3, startAmount)-startAmount
+				strat4 = a.test_strategy(strategy4, startAmount)-startAmount
+				strat5 = a.test_strategy(strategy5, startAmount)-startAmount
+				strat6 = a.test_strategy(strategy6, startAmount)-startAmount
+				strat7 = a.test_strategy(strategy7, startAmount)-startAmount
+				print("{} | Returns from buy and hold: ${:,.2f}".format(stock, buyAndHold))
+				print("{} | Returns from Strategy 1: ${:,.2f}".format(stock, strat1))
+				print("{} | Returns from Strategy 3: ${:,.2f}".format(stock, strat3))
+				print("{} | Returns from Strategy 4: ${:,.2f}".format(stock, strat4))
+				print("{} | Returns from Strategy 5: ${:,.2f}".format(stock, strat5))
+				print("{} | Returns from Strategy 6: ${:,.2f}".format(stock, strat6))
+				print("{} | Returns from Strategy 7: ${:,.2f}".format(stock, strat7))
+				info = []
+				info.append({'name': 'strat1','count': strat1})
+				info.append({'name': 'strat3','count': strat3})
+				info.append({'name': 'strat4','count': strat4})
+				info.append({'name': 'strat5','count': strat5})
+				info.append({'name': 'strat6','count': strat6})
+				info.append({'name': 'strat7','count': strat7})
+				info.append({'name':'buyAndHold', 'count': buyAndHold})
+				if max([strat1, strat3, strat4, strat5, strat6, strat7]) > buyAndHold:
+					print("WINNER")
+				print("\n")
+				newlist = sorted(info, key=lambda k: k['count'])
+				name = newlist[-1]['name']
+				if name not in infoz:
+					infoz[name] = 0
+				infoz[name] += 1
+				print infoz
+
+		except Exception as exp:
+			#print exp
+			pass
+	print info
